47404b4cca1203c00929ef7555857e6c361c9e2c
maytalor2010-web <maytalor2010@gmail.com>
2026-05-18 16:52:58 +0300

    Added a validation check for the Username. (between 3 and 20 characters, only contains "ABC" "Numbers" "." "_" )

--- website/pages/SignUp.ts
+++ website/pages/SignUp.ts
@@ -8,8 +8,35 @@ var ConfirmInput = document.querySelector<HTMLInputElement>("#confirm")!;
 var SignupButton = document.querySelector<HTMLButtonElement>("#signup")!;
 var errorDiv = get("div", "errorDiv");
 
+var validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890._";
+var validCount = 0;
+
 SignupButton.onclick = async function() {
     if (PasswordInput.value == ConfirmInput.value) {
+        
+
+        // validCount is the amount of valid characters in the Username. 
+        for(var i = 0; i<UserNameInput.value.length; i++) {
+            for(var k = 0; k<validChars.length; k++) {
+                if (UserNameInput.value[i] == validChars[k]) {
+                    validCount =+ 1;
+                }
+            }
+        }
+
+        if (validCount != UserNameInput.value.length) {
+            errorDiv.innerText == "Certain characters are invalid. Remove them and try again. ";
+        }
+        
+        if (UserNameInput.value.length < 3) {
+            errorDiv.innerText = "The Username is too short. ";
+            return;
+        }
+        else if (UserNameInput.value.length > 20) {
+            errorDiv.innerText = "The Username is too long. "
+            return;
+        }
+        
         var token = await send<string | null>("signUp", UserNameInput.value, PasswordInput.value);
         
         if (token == null) {
@@ -22,5 +49,6 @@ SignupButton.onclick = async function() {
     }
     else {
         errorDiv.innerText = "Passwords don't match. Please try again.";
+        return;
     }
 }
